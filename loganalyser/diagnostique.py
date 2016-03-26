1bc21b7e1de3a60454c20d805a65d8b3b597baae
jeremy venin <jeremy@MacBook-Pro-de-jeremy.local>
2016-03-26 22:58:43 +0100

    diagnostique selon jérémy

--- loganalyser/diagnostique.py
+++ loganalyser/diagnostique.py
@@ -1,1 +1,29 @@
+import fichier
+
+class Diagnostique:
+	"""classe servant à réaliser les analyse"""
+
+	def __statistique__(self,token_dic):
+		"""effectue des calculs statistiques sur les token
+		:param token_dic: dictionnaire de token
+		:type token_dic: Token Dictionnary"""
+		return
+
+	def __analyse_multitoken__(self,token_dic,regexp_dic):
+		"""analyse les tokens par groupe selon certains motifs
+		:param token_dic: dictionnaire de token
+		:param regex_dic: dictionnaire d'expression régulière
+		:type token_dic: Token dictionnary
+		:type regexp_dic: Regexp dictionnary"""
+		return
+		
+	def getreport(self,fileformat)
+		"""ordonne les donnée issues des statistiques et des analyses
+		:param fileformat: indique si le rapport doit etre enregistré sous forme de fchier texte ou seulement être présenté dans la console
+		:type fileformat: Boolean
+		:return: tableau des lignes de résultats à partir des dictionnaires 
+		:rtype: String array"""
+		self.report=[]
+		return(self.report)
+
 
