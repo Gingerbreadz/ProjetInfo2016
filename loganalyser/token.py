749c6519340df4c68c35d5d2241b789a0844bdb1
jeremy venin <jeremy@MacBook-Pro-de-jeremy.local>
2016-03-28 00:50:36 +0200

    many things to improve here

--- loganalyser/token.py
+++ loganalyser/token.py
@@ -1,7 +1,37 @@
 from abc import ABCMeta, abstractmethod
 
-
-class Token:
+#pas du tout fini, juste un début de travail à modifier et compléter
+class Token():
     """Classe abstraite interface pour token caractérisé par :
     - """
     __metaclass__ = ABCMeta
+
+	def getData():
+		return
+
+	def getState():
+		return
+	
+	def analyse():
+		return
+
+class IP(Token):
+	
+
+class Name(Token:
+
+
+class Date(Token):
+
+class EXT(Token):
+
+class Methode(Token):
+
+class URL(Token):
+
+class Response(Token):
+
+class Byte(Token):
+
+class Referer(Token):
+
