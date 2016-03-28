4a6ef28d46e7f7a8951f75829f7ec0282929f445
gingerbreadz <anatole.lefort@gmail.com>
2016-03-28 13:17:59 +0200

    added constructor and improved class structure

--- loganalyser/diagnostique.py
+++ loganalyser/diagnostique.py
@@ -1,29 +1,56 @@
-import fichier
+from loganalyser import token   #Gestion des tokens
+from loganalyser import outils  #Gestion des Dictionnaires
+
 
 class Diagnostique:
-	"""classe servant à réaliser les analyse"""
+    """Classe instanciant le diagnostique, qui contient les résultats d'analyse et les fait.
+    """
 
-	def __statistique__(self,token_dic):
-		"""effectue des calculs statistiques sur les token
-		:param token_dic: dictionnaire de token
-		:type token_dic: Token Dictionnary"""
-		return
+    def __init__(self, token_dictionary, regexp_dictionary):
+        """Constructeur de classe. Un diagnostique est initialisé à partir de tokens.
+            :param token_dictionary: dictionnaire de token
+            :type token_dictionary: dict
+            :param regexp_dictionary: dictionnaire de regexp
+            :type regexp_dictionary: dict"""
 
-	def __analyse_multitoken__(self,token_dic,regexp_dic):
-		"""analyse les tokens par groupe selon certains motifs
-		:param token_dic: dictionnaire de token
-		:param regex_dic: dictionnaire d'expression régulière
-		:type token_dic: Token dictionnary
-		:type regexp_dic: Regexp dictionnary"""
-		return
-		
-	def getreport(self,fileformat)
-		"""ordonne les donnée issues des statistiques et des analyses
-		:param fileformat: indique si le rapport doit etre enregistré sous forme de fchier texte ou seulement être présenté dans la console
-		:type fileformat: Boolean
-		:return: tableau des lignes de résultats à partir des dictionnaires 
-		:rtype: String array"""
-		self.report=[]
-		return(self.report)
+        self.token_dict = token_dictionary
+        self.regexp_dict = regexp_dictionary
+        self.stat_dict = {}
+        self.attack_dict = {}
+        self.__statistique()
+        self.__analyse_multitoken()
+        return
+
+    def __statistique(self):
+        """Effectue des calculs statistiques sur les token.
+        :param token_dict: dictionnaire de token
+        :type token_dict: dict
+        :return: Dictionnaire contenant les statistiques
+        :rtype: dict"""
+        self.stat_dict = {}
+        return
+
+    def __analyse_multitoken(self):
+        """Analyse les tokens par groupe selon certains motifs.
+        :param token_dict: dictionnaire de token
+        :param regexp_dict: dictionnaire d'expression régulière
+        :type token_dict: dict
+        :type regexp_dict: dict
+        :return: Dictionnaire contenant le rapport des attaques subit
+        :rtype: dict"""
+        self.attack_dict = {}
+        return
+
+    def get_report(self, fileformat):
+        """Ordonne les donnée issues des statistiques et des analyses, prépare pour l'affichage finale.
+        :param fileformat: indique si le rapport doit etre enregistré sous forme de fchier texte ou seulement être présenté dans la console
+        :type fileformat: bool
+        :return: tableau des lignes de résultats à partir des dictionnaires
+        :rtype: String array"""
+        if fileformat:
+            report = ["Ceci est la première ligne du Fichier Rapport", "Ceci est la deuxième"]
+        else:
+            report = ["===Attack===", self.attack_dict.__str__(), "===Stats===", self.stat_dict.__str__()]
+        return report
 
 
