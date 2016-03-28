fc05afd7c569ff9ac7a232ad43b42cd39ed9599b
gingerbreadz <anatole.lefort@gmail.com>
2016-03-28 22:36:57 +0200

    docs initialisée

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../loganalyser'))
 
 # -- General configuration ------------------------------------------------
 
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Apache Log Analyser'
+project = 'Apache LogAnalyser'
 copyright = '2016, Jeremy Venin, Clément Aubry, Antoine Tadros, Anatole Lefort'
 author = 'Jeremy Venin, Clément Aubry, Antoine Tadros, Anatole Lefort'
 
