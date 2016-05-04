111a5f2be4e6c04a922d6d4daf19d04a8d444780
gingerbreadz <anatole.lefort@gmail.com>
2016-05-04 18:36:55 +0200

    added TODO

--- loganalyser/diagnostique.py
+++ loganalyser/diagnostique.py
@@ -1,6 +1,7 @@
 """
 Implementation du Diagnostique.
 Il reste à écrire les méthode d'analyse et de statistique.
+TODO: Ecrire la méthode qui génère le rapport.
 """
 
 from loganalyser import token   #Gestion des tokens
