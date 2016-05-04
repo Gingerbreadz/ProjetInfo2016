2333b61cbf4d47c375cd8b2573c0ef0e6974e87a
gingerbreadz <anatole.lefort@gmail.com>
2016-05-04 18:44:44 +0200

    added TODO

--- loganalyser/main.py
+++ loganalyser/main.py
@@ -2,4 +2,5 @@
 Boucle principale
 Pour l'instant vide
 C'est ici que seront implémentées les intéractions avec l'utilisateur et utilisées les classes/méthodes des autres modules.
+TODO: écrire démarche globale du programme. Ajouter Interfaces avec les inputs (arguments) et les outputs (std_out) | Pas de Reste.
 """
