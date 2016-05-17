d457346eb141b216afa00569a55e44bfd5b6d100
gingerbreadz <anatole.lefort@gmail.com>
2016-05-17 21:32:24 +0200

    package now ready to be installed

--- setup.py
+++ setup.py
@@ -5,21 +5,16 @@ La base est là, mais il faudra se pencher sur quoi rajouter/modifier pour avoir
 
 from setuptools import setup, find_packages
 
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='loganalyser',
     version='0.0.1',
     description='Apache log file analyser',
-    long_description=readme,
     #author='',
     #author_email='',
     url='https://github.com/Gingerbreadz/ProjetInfo2016',
-    #license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    license="TSP",
+    packages=['loganalyser'],
+    entry_points={
+        "console_scripts": ['loganalyser = loganalyser.__main__:main']
+    },
 )
