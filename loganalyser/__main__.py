6e773a2d8b88e3522121cfda2c097981b25bf15d
gingerbreadz <anatole.lefort@gmail.com>
2016-05-17 18:59:37 +0200

    added token class support

--- loganalyser/__main__.py
+++ loganalyser/__main__.py
@@ -9,17 +9,32 @@ import sys
 from loganalyser import fichier
 from loganalyser import outils
 from loganalyser import diagnostique
+from loganalyser import token
 
 out = sys.stdout
-tokenlist = ["IP", "Name", "Date", "Ext", "Methode", "URL", "Response", "Byte", "Referrer"]
+tokenkeys = ["IP", "Name", "Date", "Ext", "Methode", "URL", "Response", "Byte", "Referrer"]
 
+
+def listtotokenlist(list):
+    s = list.__init__()
+    s.append(token.IP(list[0]))
+    s.append(token.Name(list[1]))
+    s.append(token.Date(list[2]))
+    s.append(token.EXT(list[3]))
+    s.append(token.Methode(list[4]))
+    s.append(token.URL(list[5]))
+    s.append(token.Response(list[6]))
+    s.append(token.Byte(list[7]))
+    s.append(token.Referer(list[8]))
+    return s
 
 def recuperertokens(cheminfichier):
-    log_dic = outils.Dictionary(tokenlist)
+    log_dic = outils.Dictionary(tokenkeys)
     fichierdelog = fichier.FichierDeLog(cheminfichier)
     for i in range(0, fichierdelog.nbLigne):
         ligne = fichierdelog.decouperligne(i)
-        log_dic.addentry(ligne)
+        tokenlist = listtotokenlist(ligne)
+        log_dic.addentry(tokenlist)
     return log_dic
 
 
