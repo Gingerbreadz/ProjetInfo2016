a742e2b57f68f46710fc206e91d1aafbec4edd6d
Antoine Tadros <tadros.antoine1@gmail.com>
2016-05-21 21:57:49 +0200

    création fichier test pour tokens

---
+++ tests/test_basic_tokens.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+test pour la classe Token
+"""
+
+import sys
+path = "/home/antoine/ProjetInfo/ProjetInfo2016"   #à retirer pour la version finale
+sys.path.append(path)
+from loganalyser import token as t
+
+
