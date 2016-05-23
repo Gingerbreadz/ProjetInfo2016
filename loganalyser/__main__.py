c3d1e0601965a36f8d0a44a735807cc9bafc8d4d
Clmx77 <pro.aubry@gmail.com>
2016-05-23 09:06:53 +0200

    Update __main__.py

--- loganalyser/__main__.py
+++ loganalyser/__main__.py
@@ -52,7 +52,7 @@ def recupererregexp(cheminfichier):
 
 def main():
     n = int(input("combien de lignes à afficher ?\n"))
-    logfilepath = str(sys.argv[0])
+    logfilepath = "./res/wwwassos-access.log"
     regexpfilepath = "./res/default_filter.xml"
     log_dic = recuperertokens(logfilepath)
     regexp_dic = recupererregexp(regexpfilepath)
