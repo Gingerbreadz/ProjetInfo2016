5c8cce8b7af100226f861ffcb1d467ba012a25c4
Clmx77 <pro.aubry@gmail.com>
2016-05-23 04:52:10 +0200

    Update diagnostique.py

--- loganalyser/diagnostique.py
+++ loganalyser/diagnostique.py
@@ -211,7 +211,7 @@ class Diagnostique:
         L = []
         for url, e in L2[:5]:
             L.append("URL " + str(url) + "\n\t" + "Impact\t\t" + str(e[3]) + "\n\tDescription\t" + str(e[2]) + "\n\tLogLineNumbers\t" + ", ".join(e[1]) + "\n\n")
-        L3 = sorted(d.items(), key=lambda e: (-e[1][0], -e[1][3]))
+        L3 = sorted(d.items(), key=lambda e: (-e[1][3], -e[1][0]))
         for url, e in L3[:5]:
             L.append("URL " + str(url) + "\n\t" + "Impact\t\t" + str(e[3]) + "\n\tDescription\t" + str(e[2]) + "\n\tLogLineNumbers\t" + ", ".join(e[1]) + "\n\n")
         return L
