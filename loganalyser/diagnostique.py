f22d0c7007a971c94697622e7319909ac2864504
Clmx77 <pro.aubry@gmail.com>
2016-05-23 04:42:19 +0200

    Update diagnostique.py

--- loganalyser/diagnostique.py
+++ loganalyser/diagnostique.py
@@ -210,7 +210,7 @@ class Diagnostique:
         L2 = sorted(d.items(), key=lambda e: -e[1][0]) # tri le dictionnaire en fonction du nombre d'apparition décroissant (d'ou le signe negatif)
         L = []
         for url, e in L2[:5]:
-            L.append("Impact\t" + str(e[3]) + "\tDescription\t" + str(e[2]) + "\n\t" + "URL\t" + str(url) + "\n\t" + "LogLineNumbers\t" + ", ".join(e[1]) + "\n\n")
+            L.append("URL\t" + str(url) + "\n\t" + "Impact\t" + str(e[3]) + "\n\tDescription\t" + str(e[2]) + "\n\tLogLineNumbers\t" + ", ".join(e[1]) + "\n\n")
         return L
         
     
